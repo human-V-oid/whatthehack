b49674a7718ee6541ea17566b8e9319f7cc8b0ec
human-V-oid <85420504+human-V-oid@users.noreply.github.com>
2025-01-10 16:05:29 +0530

    Revert "made the webpage functional"

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,7 @@
 // Get a reference to the time element
 function updateTime() {
     var today = new Date();
-    var time = today.getHours() + ":" + String(today.getMinutes()).padStart(2, '0') + ":" + String(today.getSeconds()).padStart(2, '0');
+    var time = today.getDate() + today.getMonth() + today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
     document.getElementById("currentTime").innerHTML = time;
     setTimeout(updateTime, 1000); // Update time every second
 }
